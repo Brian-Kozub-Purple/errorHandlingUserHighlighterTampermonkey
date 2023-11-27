085124c56c3f5afaec9488d7508e2247157ee7cf
Brian Kozub <brian.ko@purple.com>
2023-11-27 12:44:16 -0700

    Highlighting the whole row, now, as well as individual names in that row. Also added a prompt for the primary and backup user names, so others can use this.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Error Handling Highlighter
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      2.0
 // @description  try to take over the world!
 // @author       You
 // @match        https://onpurple.atlassian.net/wiki/spaces/Integratio/pages/2936274945/Error+Handling
@@ -13,14 +13,35 @@
 	"use strict";
 
 	// Your code here...
-	const pCollection = document.getElementsByTagName("p");
-	for (let i = 0; i < pCollection.length; i++) {
-		if (pCollection[i].innerHTML.includes("Brian K.")) {
-			pCollection[i].style.backgroundColor = "green";
-			pCollection[i].style.color = "white";
-		}
-		if (pCollection[i].innerHTML.includes("Tony S.")) {
-			pCollection[i].style.backgroundColor = "yellow";
+	// By table columns method
+	const tables = document.getElementsByTagName("table");
+	const table = Array.from(tables).find((table) => table.rows.length > 1);
+	const targetPrimary = prompt(
+		"Who is the primary? You must type it exactly as it appears in the table. eg. 'Tony S.' or 'Gil Q.'"
+	);
+	const targetSupport = prompt(
+		"Who is the backup? You must type it exactly as it appears in the table. eg. 'Brian K.' or 'Jose C.'"
+	);
+	for (let i = 0; i < table.rows.length; i++) {
+		const currentRow = table.rows[i];
+		const primaryCell = currentRow.cells[2];
+		const primaryName = primaryCell.firstChild.innerHTML;
+		const secondaryCell = currentRow.cells[3];
+		const secondaryName = secondaryCell.firstChild.innerHTML;
+		const tertiaryCell = currentRow.cells[4];
+		const tertiaryName = tertiaryCell.firstChild.innerHTML;
+
+		if (primaryName == targetPrimary) {
+			primaryCell.style.backgroundColor = "white";
+			if (secondaryName == targetSupport || tertiaryName == targetSupport) {
+				currentRow.style.backgroundColor = "#CED9EC";
+				if (secondaryName == targetSupport) {
+					secondaryCell.style.backgroundColor = "lightgray";
+				}
+				if (tertiaryName == targetSupport) {
+					tertiaryCell.style.backgroundColor = "darkgray";
+				}
+			}
 		}
 	}
 })();
